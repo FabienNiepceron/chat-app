ca736a5cc6c5d7386dae3b89a4377b322cdbf134
FabienNiepceron <fabienniepceron72@gmail.com>
2020-11-09 14:51:03 +0100

    Exercise ends

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from "react";
 import "./App.css";
-// import Contact from "./components/Contact.js";
+import Contact from "./components/Contact.js";
 import ContactList from "./components/ContactList";
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
     <div className="App">
-      {
-        /* <Contact
-        avatar="https://randomuser.me/api/portraits/women/54.jpg"
-        name="Anita Sutton"
-        online={true}
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/men/1.jpg"
-        name="Jim George"
-        online={false}
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/7.jpg"
-        name="Charlotte Ryan"
-        online={true}
-      /> */
-        <ContactList />
-      }
-    </div>
-  );
+      return
+      <ContactList />
+    </div>;
+  }
 }
-
 export default App;
